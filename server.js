fc032c1631e7234707cacae2af6b615e42ca9bc6
Kean Walmsley <kean.walmsley@autodesk.com>
2015-05-26 14:27:34 -0700

    Use index.html

--- server.js
+++ server.js
@@ -59,10 +59,6 @@ app.use(function (req, res, next) {
 
 app.use('/', express.static(__dirname + '/html'));
 
-app.get('/', (function (req, res) {
-  res.sendFile(path.join(__dirname, '/html/', 'jigsite.html'));
-}));
-
 /////////////////////////////////////////////////////////////////////////////////
 //  Rest API
 //
