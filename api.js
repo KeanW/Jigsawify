331eb97b963c3b6cf89672d47c465cee686f4bbb
Kean Walmsley <kean.walmsley@autodesk.com>
2015-07-30 12:18:24 +0200

    Trying to force protocol

--- api.js
+++ api.js
@@ -25,7 +25,7 @@ exports.submitData = function (req, res) {
   var reqId = randomValueBase64(6);
   res.end(reqId);
   
-  siteUrl = '//' + req.get('host');
+  siteUrl = 'https://' + req.get('host');
   
   var args = url.parse(req.url, true).query;
 
