55576a4560e711535d7b9f6673b04375d10ac294
Kean Walmsley <kean.walmsley@autodesk.com>
2015-07-30 14:07:56 +0200

    Fixed multiple spinner problem

--- html/js/jigsite.js
+++ html/js/jigsite.js
@@ -2,6 +2,7 @@ var stage = 0;
 var pixelsWidth = 300;
 var uploadedBlob = undefined;
 var already = undefined;
+var spinner = undefined;
 
 var stageText =
   [
@@ -90,76 +91,91 @@ function setStage(newStage) {
 
   // Some custom logic to be run for the stages that need it
 
-  if (newStage == 0) {
-    $('#content')[0].style.width = '640px';
-
-    // Clear the engraved image, reset the slider
-
-    var canvas = $('#rawData2')[0];
-    if (canvas) {
-      canvas.height = 0;
-      $('#threshold')[0].value = 70;
-    }
-  }
-
-  if (newStage == 1) {
-    $('#content')[0].style.width = '100%';
-    setTimeout(function(){
-      var img = $('#image')[0];
-      var canvas = buildCanvas(img, 'rawData');
-      var outcanvas = buildCanvas(img);
-      edgeDetector.init(img, canvas, outcanvas);
-      edgeDetector.update(edgeDetector.threshold);
-    }, 0);
-  }
-
-  if (newStage == 2) {
-    $('#content')[0].style.width = '640px';
-    setTimeout(function(){
-      var img = $('#image')[0];
-      var canvas = buildCanvas(img, 'rawData2', $('#engimage')[0]);
-      var outcanvas = buildCanvas(img);
-      edgeDetector.init(img, canvas, outcanvas);
-      edgeDetector.update(edgeDetector.threshold);
-      if (already) {
-        // This will keep the proportions of the new image
-        setWidth($('#width').val());
+  switch (newStage) {
+    case 0:
+      $('#content')[0].style.width = '640px';
+  
+      // Clear the engraved image, reset the slider
+  
+      var canvas = $('#rawData2')[0];
+      if (canvas) {
+        canvas.height = 0;
+        $('#threshold')[0].value = 70;
+      }
+      break;
+  
+    case 1:
+      $('#content')[0].style.width = '100%';
+      setTimeout(function(){
+        var img = $('#image')[0];
+        var canvas = buildCanvas(img, 'rawData');
+        var outcanvas = buildCanvas(img);
+        edgeDetector.init(img, canvas, outcanvas);
+        edgeDetector.update(edgeDetector.threshold);
+      }, 0);
+      break;
+  
+    case 2:
+      $('#content')[0].style.width = '640px';
+      setTimeout(function(){
+        if (spinner)
+          spinner.stop();
+        
+        var img = $('#image')[0];
+        var canvas = buildCanvas(img, 'rawData2', $('#engimage')[0]);
+        var outcanvas = buildCanvas(img);
+        edgeDetector.init(img, canvas, outcanvas);
+        edgeDetector.update(edgeDetector.threshold);
+        if (already) {
+          // This will keep the proportions of the new image
+          setWidth($('#width').val());
+        } else {
+          setWidth(12);
+          setPieces(100);
+          already = true;
+        }
+      }, 0);
+      break;
+  
+    case 3:
+  
+      // Only enter the waiting stage if going forward
+  
+      if (stage == 2) {
+  
+        // Create our HTML spinner
+        // (from http://fgnass.github.io/spin.js)
+  
+        if (spinner) {
+          spinner.spin($('#loading')[0]);
+        } else {
+          var opts = {
+            lines: 13, length: 28, width: 14, radius: 42, scale: 1.5,
+            corners: 1, color: '#fff', opacity: 0.25, rotate: 0,
+            direction: 1, speed: 1, trail: 60, fps: 20, zIndex: 2e9,
+            className: 'spinner', top: '50%', left: '50%', shadow: false,
+            hwaccel: false, position: 'absolute'
+          }
+          spinner = new Spinner(opts);
+          spinner.spin($('#loading')[0]);
+        }
+  
+        process();
+  
       } else {
-        setWidth(12);
-        setPieces(100);
-        already = true;
+  
+        // Otherwise we skip it
+  
+        setStage(2);
+        return;
       }
-    }, 0);
-  }
-
-  if (newStage == 3) {
-
-    // Only enter the waiting stage if going forward
-
-    if (stage == 2) {
-
-      // Create our HTML spinner
-      // (from http://fgnass.github.io/spin.js)
-
-      var opts = {
-        lines: 13, length: 28, width: 14, radius: 42, scale: 1.5,
-        corners: 1, color: '#fff', opacity: 0.25, rotate: 0,
-        direction: 1, speed: 1, trail: 60, fps: 20, zIndex: 2e9,
-        className: 'spinner', top: '50%', left: '50%', shadow: false,
-        hwaccel: false, position: 'absolute'
-      }
-      var target = document.getElementById('loading');
-      var spinner = new Spinner(opts).spin(target);
-
-      process();
-
-    } else {
-
-      // Otherwise we skip it
-
-      setStage(2);
-      return;
-    }
+      break;
+   
+    case 4:
+      if (spinner)
+        spinner.spin();
+      break;
+    default:
   }
 
   // Then we just loop through the elements, switching the ones
