b206cd6061745506665a6d0fee423706b88320f7
Kean Walmsley <kean.walmsley@autodesk.com>
2015-08-21 21:54:06 +0200

    Made the download size human-readable

--- html/js/jigsite.js
+++ html/js/jigsite.js
@@ -260,12 +260,26 @@ function setLinkAndSizeTooltip(id, url) {
   findSize(url, function(size) {
     var elem = $(id);
     elem.attr('onclick', 'window.location.href="' + url + '"');
-    elem.tooltip(
-      { placement: window.innerWidth < 681 ? "bottom" : "right",
-        title: size
-      }
-    ); 
+    elem.tooltip( { placement: "top", title: humanFileSize(size, false) } );
   });
+}
+
+// From: http://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable
+
+function humanFileSize(bytes, si) {
+  var thresh = si ? 1000 : 1024;
+  if(Math.abs(bytes) < thresh) {
+    return bytes + ' B';
+  }
+  var units = si
+    ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+    : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+  var u = -1;
+  do {
+    bytes /= thresh;
+    ++u;
+  } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+  return bytes.toFixed(1)+' '+units[u];
 }
 
 function findSize(url, success) {
